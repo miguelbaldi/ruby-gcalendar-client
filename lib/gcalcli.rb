8b45c1eaf914fa4b5cc9c6be369cb48a93b5c4d2
miguelbaldi <miguel.baldi@1up4dev.org>
2009-09-22 20:00:28 -0300

    DateTime format

--- lib/gcalcli.rb
+++ lib/gcalcli.rb
@@ -39,7 +39,7 @@ module CalendarReader
     def add_event(event, sortit=true)
       self.events = [] unless self.events.is_a?(Array)
       self.events << event
-      @events.sort! {|a,b| a.start_time <=> b.start_time } if sortit
+      @events.sort! {|a,b| a.dtstart <=> b.dtstart } if sortit
       event
     end
 
@@ -68,7 +68,9 @@ module CalendarReader
 
       # Now you can access the cal object in just the same way I created it
       cal.events.each do |event|
-        self.events << 
+        self.add_event(event, false) # (disable sorting until done)
+        @events.reject! {|e| e.dtstart.nil?}
+        @events.sort! {|a,b| a.dtstart <=> b.dtstart }
       end
     end
     def parse_from_ical
@@ -80,17 +82,17 @@ module CalendarReader
     end
 
     def future_events
-      t = Time.now
-      events.inject([]) {|future,e| e.start_time > t ? future.push(e) : future}
+      t = DateTime.now
+      events.inject([]) {|future,e| e.dtstart > t ? future.push(e) : future}
     end
 
     def past_events
-      t = Time.now
-      events.inject([]) {|past,e| e.start_time < t ? past.push(e) : past}
+      t = DateTime.now
+      events.inject([]) {|past,e| e.dtstart < t ? past.push(e) : past}
     end
 
     def events_in_range(start_time, end_time)
-      events.inject([]) {|in_range,e| e.start_time < end_time && e.end_time > start_time ? in_range.push(e) : in_range}
+      events.inject([]) {|in_range,e| e.dtstart < end_time && e.dtend > start_time ? in_range.push(e) : in_range}
     end
 
     def calendar_raw_data
@@ -107,37 +109,18 @@ module CalendarReader
           return nil
         end
       else
-        Net::HTTP.start('www.google.com', 80) do |http|
-          response = http.get(self.url)
-          case response
-          when Net::HTTPSuccess, Net::HTTPRedirection
-            #puts "Data #{data}"
-            return response.body
-          else
-            response.error!
-            return nil
-          end
-        end        
+        response = Net::HTTP.get_response(URI.parse(self.url))
+        case response
+        when Net::HTTPSuccess, Net::HTTPRedirection
+          #puts "Data #{data}"
+          return response.body
+        else
+          response.error!
+          return nil
+        end      
       end
     end
 
   end
 
 end
-=begin
-include CalendarReader
-
-unless ARGV.length > 0 
-   puts "Numero de parametros invalido."
-   puts "Uso: ruby gcalcli.rb <url_feed_google_calendar> \n"
-   puts "Exemplo: ruby gcalcli.rb /calendar/ical/miguel.horlle%40gmail.com/private-e53bc30e66f953cbw3574yf619b5451/basic.ics \n"
-   exit
-end
-
-gcal_url = ARGV[0]
-
-g = Calendar.new(gcal_url)
-puts "Events length #{g.events.length}"
-puts "Events future_events #{g.future_events.length}"
-g.events_in_range(Time.now - 432000, Time.now + 500000).collect {|e| puts "Event\n #{e.summary} e Description #{e.description}"}
-=end
