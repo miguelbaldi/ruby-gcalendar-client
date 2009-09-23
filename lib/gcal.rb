22a503316739a5b29c181e173162a080810781cc
miguelbaldi <miguel.baldi@1up4dev.org>
2009-09-22 23:08:13 -0300

    Removing unnecessary file

--- lib/gcal.rb
+++
@@ -1,5 +0,0 @@
-include CalendarReader
-g = Calendar.new('http://www.google.com/calendar/ical/miguel.horlle%40gmail.com/private-e53bc30e66f953cb535747f6d19b5451/basic.ics')
-g.events.length
-g.future_events.length
-g.events_in_range(5.days.ago, 5.days.from_now).collect {|e| e.summary}
